a381ec5ef8e277cb089b4150a04da34c3067aa16
fesprojeto2018 <38480825+fesprojeto2018@users.noreply.github.com>
2018-09-27 17:41:56 -0300

    Telas de login e cadastro + classe usuario

---
+++ SendWithMe/app/src/main/java/com/example/emano/sendwithme/Usuario.java
@@ -0,0 +1,41 @@
+package com.example.emano.sendwithme;
+
+public class Usuario {
+
+    String nome;
+    String email;
+    String cpf;
+    String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
